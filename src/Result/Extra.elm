a9b33fd2d431f02340b64dd192f4322fb02e82bb
Richard Feldman <rtfeldman@users.noreply.github.com>
2015-08-04 11:38:37 -0700

    Initial commit

---
+++ src/Result/Extra.elm
@@ -0,0 +1,26 @@
+module Result.Extra (withDefault) where
+{-| Convenience functions for working with Results
+
+# Defaults
+@docs withDefault
+
+-}
+
+import Regex
+
+
+{-| Returns a `Result`'s contents if the `Result` is an `Ok`,
+or the given default value if the `Result` is an `Err`.
+
+This is basically the same as running `Result.toMaybe` and then `Maybe.withDefault`.
+
+    0 == Result.withDefault 0 (String.toInt "this is not a valid integer!")
+-}
+withDefault : a -> Result x a -> a
+withDefault default result =
+    case result of
+        Ok value ->
+            value
+
+        Err _ ->
+            default
